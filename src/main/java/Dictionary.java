9e7365f106276b1200c89c295d12688318f3d526
ias4 <ias4@pc5-037-l.cs.st-andrews.ac.uk>
2019-02-17 19:10:28 +0000

    Adding unit tests for the Dictionary class. Checking all input file input cases. The program allows the user to input number, special characters etc, while the other situations (empty lines, white spaces) are properly dealt with. The test for the file not found exception is commented. The reason behind this is that, I'm still looking for the best practice on how to check for that. Will be solved soon.

--- src/main/java/Dictionary.java
+++ src/main/java/Dictionary.java
@@ -3,42 +3,48 @@ import java.util.ArrayList;
 
 public class Dictionary {
 
-	static String[] countyNames = { "Argyll and Bute", "Caithness",  "Kingdom of Fife",
-			            "East Lothian", "Highland", "Dumfries and Galloway",
-			            "Renfrewshire", "Scottish Borders", "Perth and Kinross" };
-	static String[] countryNames = { "Scotland", "England", "Wales", "Northern Ireland", "Ireland",
-			            "France", "Germany", "Netherlands", "Spain", "Portugal",
-			            "Belgium", "Luxembourg", "Switzerland", "Italy", "Greece" };
-	static String[] cityNames = { "St Andrews", "Edinburgh", "Glasgow", "Kirkcaldy", "Perth",
-			            "Dundee", "Stirling", "Inverness", "Aberdeen", "Falkirk" };
-			
-	static ArrayList<String> customNames;
-	
-	public static String getRandomName(int category) {
-		if (category == 1)
-			return countyNames[(int)(Math.random()*9)];
-		if (category == 2)
-			return countryNames[(int)(Math.random()*15)];
-		return cityNames[(int)(Math.random()*10)];
-	}
-	
-	public static String getRandomCustomName(String wordsource) {
-		String line;
-		customNames = new ArrayList<String>();
-		
-		try {
-			FileReader file = new FileReader(wordsource);
-			BufferedReader reader = new BufferedReader(file);
-			while((line = reader.readLine()) != null) {
-				customNames.add(line);
+    static String[] countyNames = {"Argyll and Bute", "Caithness", "Kingdom of Fife",
+            "East Lothian", "Highland", "Dumfries and Galloway",
+            "Renfrewshire", "Scottish Borders", "Perth and Kinross"};
+    static String[] countryNames = {"Scotland", "England", "Wales", "Northern Ireland", "Ireland",
+            "France", "Germany", "Netherlands", "Spain", "Portugal",
+            "Belgium", "Luxembourg", "Switzerland", "Italy", "Greece"};
+    static String[] cityNames = {"St Andrews", "Edinburgh", "Glasgow", "Kirkcaldy", "Perth",
+            "Dundee", "Stirling", "Inverness", "Aberdeen", "Falkirk"};
+
+    static ArrayList<String> customNames;
+
+    public static String getRandomName(int category) {
+        if (category == 1)
+            return countyNames[(int) (Math.random() * 9)];
+        if (category == 2)
+            return countryNames[(int) (Math.random() * 15)];
+        return cityNames[(int) (Math.random() * 10)];
+    }
+
+    public static String getRandomCustomName(String wordSource){
+        String line;
+        customNames = new ArrayList<String>();
+
+        try {
+            FileReader file = new FileReader(wordSource);
+            BufferedReader reader = new BufferedReader(file);
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+                if (!line.isEmpty()) {
+                    customNames.add(line);
+                }
             }
-			return customNames.get((int)(Math.random()*customNames.size()));
-		} catch(FileNotFoundException e) {
-			System.out.println("File error");
-			return "";
-		} catch(IOException e) {
-		System.out.println("IO error");
-		return "";
-	}
-	}
+            if(customNames.size() == 0){
+                return "";
+            }
+            return customNames.get((int) (Math.random() * customNames.size()));
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+            return "";
+        } catch (IOException e) {
+            System.out.println("IO ERROR");
+            return "";
+        }
+    }
 }
--- src/test/java/DictionaryTest.java
+++ src/test/java/DictionaryTest.java
@@ -2,6 +2,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class DictionaryTest {
@@ -54,5 +57,113 @@ public class DictionaryTest {
         assertTrue(Arrays.asList(Dictionary.cityNames).contains(cityName));
     }
 
+    @Test
+    public void getRandomCustomNameTestOne() {
+        String wordSource = "oneName.txt";
+        String targetName = Dictionary.getRandomCustomName(wordSource);
+        assertEquals(targetName, "London");
+        assertEquals(Dictionary.customNames.size(), 1);
+    }
+
+    @Test
+    public void getRandomCustomNameTestTwo() {
+        String wordSource = "twoNames.txt";
+        ArrayList<String> possibleNames = new ArrayList<>();
+        possibleNames.add("London");
+        possibleNames.add("Edinburgh");
+
+        String targetName = Dictionary.getRandomCustomName(wordSource);
+
+        assertEquals(Dictionary.customNames.size(), 2);
+        assertEquals(Dictionary.customNames, possibleNames);
+        assertTrue(possibleNames.contains(targetName));
+    }
+
+    @Test
+    public void specialCharactersGetRandomCustomNameTest() {
+        String wordSource = "specialCharacters.txt";
+        ArrayList<String> possibleNames = new ArrayList<>();
+        possibleNames.add("123%");
+        possibleNames.add("*****");
+
+        String targetName = Dictionary.getRandomCustomName(wordSource);
+
+        assertEquals(Dictionary.customNames.size(), 2);
+        assertEquals(Dictionary.customNames, possibleNames);
+        assertTrue(possibleNames.contains(targetName));
+    }
+
+    @Test
+    public void emptyLineGetRandomCustomNameTest() {
+        String wordSource = "emptyLine.txt";
+        ArrayList<String> possibleNames = new ArrayList<>();
+        possibleNames.add("London");
+        possibleNames.add("Edinburgh");
+
+        String targetName = Dictionary.getRandomCustomName(wordSource);
+
+        assertEquals(Dictionary.customNames.size(), 2);
+        assertEquals(Dictionary.customNames, possibleNames);
+        assertTrue(possibleNames.contains(targetName));
+    }
+
+    @Test
+    public void whiteSpaceLineGetRandomCustomNameTest() {
+        String wordSource = "whiteSpace.txt";
+        ArrayList<String> possibleNames = new ArrayList<>();
+        possibleNames.add("London");
+        possibleNames.add("Edinburgh");
+
+        String targetName = Dictionary.getRandomCustomName(wordSource);
+        //System.out.println(Arrays.asList(Dictionary.customNames));
+
+        assertEquals(Dictionary.customNames.size(), 2);
+        assertEquals(Dictionary.customNames, possibleNames);
+        assertTrue(possibleNames.contains(targetName));
+    }
+
+    @Test
+    public void whiteSpaceInNameGetRandomCustomNameTest() {
+        String wordSource = "whiteSpace2.txt";
+        ArrayList<String> possibleNames = new ArrayList<>();
+        possibleNames.add("London");
+        possibleNames.add("Edinburgh");
+
+        String targetName = Dictionary.getRandomCustomName(wordSource);
+
+        assertEquals(Dictionary.customNames.size(), 2);
+        assertEquals(Dictionary.customNames, possibleNames);
+        assertTrue(possibleNames.contains(targetName));
+    }
+
+    @Test
+    public void sameLineNameGetRandomCustomNameTest() {
+        String wordSource = "sameLine.txt";
+
+        String targetName = Dictionary.getRandomCustomName(wordSource);
+
+        assertEquals(targetName, "Los Angeles");
+        assertEquals(Dictionary.customNames.size(), 1);
+    }
+
+    @Test
+    public void emptyFileGetRandomCustomNameTest() {
+        String wordSource = "emptyFile.txt";
+
+        String targetName = Dictionary.getRandomCustomName(wordSource);
+
+        assertEquals(targetName, "");
+        assertEquals(Dictionary.customNames.size(), 0);
+    }
+
+    /*@Test(expected = FileNotFoundException.class)
+    public void wrongFileGetRandomCustomNameTest() {
+        String wordSource = "asd";
+
+        String targetName = Dictionary.getRandomCustomName(wordSource);
+
+        assertEquals(targetName, "");
+        assertEquals(Dictionary.customNames.size(), 0);
+    }*/
 
 }
