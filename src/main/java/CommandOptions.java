ecc0db31b113966ab7756904fc3513d59e69a66b
ias4 <ias4@pc5-037-l.cs.st-andrews.ac.uk>
2019-02-17 14:46:22 +0000

    Taking a step-by-step structural approach to refactoring the code. Checking for the user's command line input (guesses, hints, input file). I started by writing unit tests to check for wnated functionality and test extreme values for the number of guesses input (0, negative, letters, null etc.). Finished writing unit tests for the number of guesses. All pass succesfully! Next is to do the same for hints and input file.

--- src/main/java/CommandOptions.java
+++ src/main/java/CommandOptions.java
@@ -12,14 +12,30 @@ public class CommandOptions {
 		
 		for(int i = 0; i < args.length; ++i) {
 			if (args[i].equals("--guesses")) {
-				maxGuesses = Integer.parseInt(args[i+1]);
+				try{
+					maxGuesses = Integer.parseInt(args[i+1]);
+				} catch (NumberFormatException e){
+					System.out.println("Number of guesses has to be an integer!");
+					maxGuesses = 10;
+					throw new NumberFormatException();
+				} catch (IndexOutOfBoundsException e){
+					System.out.println("Did not specify the number of guesses! Setting guesses default value!");
+					maxGuesses = 10;
+					throw new IndexOutOfBoundsException();
+				}
+				if(maxGuesses <= 0){
+					System.out.println("Invalid input - zero or negative number detected! Setting guesses default value!");
+					maxGuesses = 10;
+				}
 				i++;
 			}
 			else if (args[i].equals("--hints")) {
 				maxHints = Integer.parseInt(args[i+1]);
 				i++;
+			} else if(args[i].equals("--file")){
+				wordsDictionary = args[i+1];
+				i++;
 			}
-			else wordsDictionary = args[i];
 		}
 	}
 }
--- src/test/java/CommandOptionsTest.java
+++ src/test/java/CommandOptionsTest.java
@@ -5,8 +5,99 @@ import org.junit.Test;
 public class CommandOptionsTest {
 
 	@Test
+	public void noInputCommandOptionsTest() {
+		String[] args = {};
+		CommandOptions commandOptions = new CommandOptions(args);
+		assertEquals(args.length, 0);
+		assertEquals(commandOptions.maxGuesses, 10);
+		assertEquals(commandOptions.maxHints, 2);
+		assertEquals(commandOptions.wordsDictionary, "");
+	}
+
+	@Test
+	public void emptyInputCommandOptionsTest() {
+		String[] args = {"", "", "", ""};
+		CommandOptions commandOptions = new CommandOptions(args);
+		assertEquals(commandOptions.maxGuesses, 10);
+		assertEquals(commandOptions.maxHints, 2);
+		assertEquals(commandOptions.wordsDictionary, "");
+	}
+
+	@Test
+	public void randomInputCommandOptionsTest() {
+		String[] args = {"asd", "120", "&&*&^", "2"};
+		CommandOptions commandOptions = new CommandOptions(args);
+		assertEquals(commandOptions.maxGuesses, 10);
+		assertEquals(commandOptions.maxHints, 2);
+		assertEquals(commandOptions.wordsDictionary, "");
+	}
+
+	@Test
+	public void guessesInputCommandOptionsTest() {
+		String[] args = {"--guesses", "7"};
+		CommandOptions commandOptions = new CommandOptions(args);
+		assertEquals(commandOptions.maxGuesses, 7);
+		assertEquals(commandOptions.maxHints, 2);
+		assertEquals(commandOptions.wordsDictionary, "");
+	}
+
+	@Test
+	public void guessesAndRandomInputCommandOptionsTest() {
+		String[] args = {"asd", "92" ,"--guesses", "7", "odo"};
+		CommandOptions commandOptions = new CommandOptions(args);
+		assertEquals(commandOptions.maxGuesses, 7);
+		assertEquals(commandOptions.maxHints, 2);
+		assertEquals(commandOptions.wordsDictionary, "");
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void nullGuessesInputCommandOptionsTest() {
+		String[] args = {"asd", "92" ,"--guesses", null, "odo"};
+		CommandOptions commandOptions = new CommandOptions(args);
+		assertEquals(commandOptions.maxGuesses, 10);
+		assertEquals(commandOptions.maxHints, 2);
+		assertEquals(commandOptions.wordsDictionary, "");
+	}
+
+	@Test
+	public void zeroGuessesInputCommandOptionsTest() {
+		String[] args = {"asd", "92" ,"--guesses", "0", "odo"};
+		CommandOptions commandOptions = new CommandOptions(args);
+		assertEquals(commandOptions.maxGuesses, 10);
+		assertEquals(commandOptions.maxHints, 2);
+		assertEquals(commandOptions.wordsDictionary, "");
+	}
+
+	@Test
+	public void negativeGuessesInputCommandOptionsTest() {
+		String[] args = {"asd", "92" ,"--guesses", "-1", "odo"};
+		CommandOptions commandOptions = new CommandOptions(args);
+		assertEquals(commandOptions.maxGuesses, 10);
+		assertEquals(commandOptions.maxHints, 2);
+		assertEquals(commandOptions.wordsDictionary, "");
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void nonNumberGuessesInputCommandOptionsTest() {
+		String[] args = {"asd", "92" ,"--guesses", "asd", "odo"};
+		CommandOptions commandOptions = new CommandOptions(args);
+		assertEquals(commandOptions.maxGuesses, 10);
+		assertEquals(commandOptions.maxHints, 2);
+		assertEquals(commandOptions.wordsDictionary, "");
+	}
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void noGuessesInputCommandOptionsTest() {
+		String[] args = {"--guesses"};
+		CommandOptions commandOptions = new CommandOptions(args);
+		assertEquals(commandOptions.maxGuesses, 10);
+		assertEquals(commandOptions.maxHints, 2);
+		assertEquals(commandOptions.wordsDictionary, "");
+	}
+
+	@Test
 	public void fileInputCommandOptionsTest() {
-		String[] args = { "--guesses", "2", "--hints", "4", "words.txt" };
+		String[] args = { "--guesses", "2", "--hints", "4", "--file", "words.txt" };
 		CommandOptions commandOptions = new CommandOptions(args);
 		assertEquals(commandOptions.maxGuesses, 2);
 		assertEquals(commandOptions.maxHints, 4);
