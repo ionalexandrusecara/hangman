d8d8de3f66ba83f0e68408119b98b40fbec6bc35
ias4 <ias4@pc2-072-l.cs.st-andrews.ac.uk>
2019-02-18 22:44:56 +0000

    Created three additional methods out of the CommandOptions constructor ('parseGuesses()', 'parseHints()', 'parseFile()'). They improve code modularity and readability.

--- src/main/java/CommandOptions.java
+++ src/main/java/CommandOptions.java
@@ -24,56 +24,67 @@ public class CommandOptions {
 		wordsDictionary = "";
 		
 		for(int i = 0; i < args.length; ++i) {
-			if (args[i].equals("--guesses")) {
-				try{
-					maxGuesses = Integer.parseInt(args[i+1]);
-				} catch (NumberFormatException e){
-					System.out.println("Number of guesses has to be an integer!");
-					throw new NumberFormatException();
-				} catch (IndexOutOfBoundsException e){
-					System.out.println("Did not specify the number of guesses! Setting guesses default value!");
-					throw new IndexOutOfBoundsException();
-				}
-
-				if(maxGuesses <= 0){
-					System.out.println("Invalid input - zero or negative number detected! Setting guesses default value!");
-					maxGuesses = 10;
-
-				}
-
-				i++;
+			switch(args[i]){
+				case "--guesses":
+					parseGuesses(args, i);
+					i++;
+					break;
+				case "--hints":
+					parseHints(args, i);
+					i++;
+					break;
+				case "--file":
+					parseFile(args, i);
+					i++;
+					break;
 			}
-			else if (args[i].equals("--hints")) {
-				try{
-					maxHints = Integer.parseInt(args[i+1]);
-				} catch (NumberFormatException e){
-					System.out.println("Number of hints has to be an integer!");
-					throw new NumberFormatException();
-				} catch (IndexOutOfBoundsException e){
-					System.out.println("Did not specify the number of hints! Setting hints default value!");
-					throw new IndexOutOfBoundsException();
-				}
+		}
+	}
 
-				if(maxHints <= 0){
-					System.out.println("Invalid input - zero or negative number detected! Setting hints default value!");
-					maxHints = 3;
-				}
+	public void parseGuesses(String[] args, int i){
+		try{
+			maxGuesses = Integer.parseInt(args[i+1]);
+		} catch (NumberFormatException e){
+			System.out.println("Number of guesses has to be an integer!");
+			throw new NumberFormatException();
+		} catch (IndexOutOfBoundsException e){
+			System.out.println("Did not specify the number of guesses! Setting guesses default value!");
+			throw new IndexOutOfBoundsException();
+		}
 
-				i++;
-			} else if(args[i].equals("--file")){
-				try{
-					wordsDictionary = args[i+1];
-				} catch (IndexOutOfBoundsException e){
-					System.out.println("Did not specify the file! Will not allow custom input!");
-					throw new IndexOutOfBoundsException();
-				}
+		if(maxGuesses <= 0){
+			System.out.println("Invalid input - zero or negative number detected! Setting guesses default value!");
+			maxGuesses = 10;
+		}
+	}
 
-				if(wordsDictionary == null){
-					wordsDictionary = "";
-				}
+	public void parseHints(String[] args, int i){
+		try{
+			maxHints = Integer.parseInt(args[i+1]);
+		} catch (NumberFormatException e){
+			System.out.println("Number of hints has to be an integer!");
+			throw new NumberFormatException();
+		} catch (IndexOutOfBoundsException e){
+			System.out.println("Did not specify the number of hints! Setting hints default value!");
+			throw new IndexOutOfBoundsException();
+		}
 
-				i++;
-			}
+		if(maxHints <= 0){
+			System.out.println("Invalid input - zero or negative number detected! Setting hints default value!");
+			maxHints = 3;
+		}
+	}
+
+	public void parseFile(String[] args, int i){
+		try{
+			wordsDictionary = args[i+1];
+		} catch (IndexOutOfBoundsException e){
+			System.out.println("Did not specify the file! Will not allow custom input!");
+			throw new IndexOutOfBoundsException();
+		}
+
+		if(wordsDictionary == null){
+			wordsDictionary = "";
 		}
 	}
 
